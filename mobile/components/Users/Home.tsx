9216fd9d35eb0262afe0f9070e338598b0c7b9dd
村山優依 <murayamayuui@murayamayuuis-MacBook-Pro.local>
2024-07-09 10:15:05 -0700

    save current work

--- mobile/components/Users/Home.tsx
+++ mobile/components/Users/Home.tsx
@@ -5,7 +5,12 @@ import {
   Heading,
   HamburgerIcon,
   HStack,
+  Text,
+  CloseIcon,
+  IconButton,
+  Icon,
 } from 'native-base';
+import { AntDesign } from "react-native-vector-icons";
 
 const Home = ({route}: any) => {
   const {firstName, lastName, email} = route.params;
@@ -13,20 +18,50 @@ const Home = ({route}: any) => {
 
   const getEmployers = () => {
     // when the page is loaded, get the all employers
-  }
-
-  const press = () => {
-    console.log('press')
-  }
+  };
 
   return (
     <NativeBaseProvider>
       <Box m="5%">
+        {menuOpen && (
+          <Box w="250px" position="absolute" top="0" right="25px">
+            {/* <Text onPress={() => console.log('hello')}>&times;</Text> */}
+            <IconButton
+              icon={<Icon as={Entypo} name="emoji-happy" />}
+              borderRadius="full"
+              _icon={{
+                color: 'orange.500',
+                size: 'md',
+              }}
+              _hover={{
+                bg: 'orange.600:alpha.20',
+              }}
+              _pressed={{
+                bg: 'orange.600:alpha.20',
+                _icon: {
+                  name: 'emoji-flirt',
+                },
+                _ios: {
+                  _icon: {
+                    size: '2xl',
+                  },
+                },
+              }}
+              _ios={{
+                _icon: {
+                  size: '2xl',
+                },
+              }}
+            />
+            <Text>See working records</Text>
+            <Text>See working conditions</Text>
+            <Text>Service Providers</Text>
+          </Box>
+        )}
+
         <HStack space={2}>
           <Box w="60%">
-            <Heading size="2xl">
-              Hi {firstName}!
-            </Heading>
+            <Heading size="2xl">Hi {firstName}!</Heading>
           </Box>
           <HamburgerIcon
             w="30%"
@@ -34,11 +69,12 @@ const Home = ({route}: any) => {
             position="relative"
             top="0"
             left="24"
-            onPress={press}
+            onPress={() => setMenuOpen(true)}
           />
         </HStack>
+
         <Box my={10}>
-            <Heading size="xs">Employer List</Heading>
+          <Heading size="xs">Employer List</Heading>
         </Box>
       </Box>
     </NativeBaseProvider>
